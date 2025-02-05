2798cadd1b8b9639edd4fda05f7698b3825bfc33
Jacob Ambroz <143131645+sixtyoneeighty@users.noreply.github.com>
2025-02-05 01:50:05 -0600

    nb

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,29 +1,17 @@
 "use client";
 
-import { clientEnv } from "@/env/client";
 import { ThemeProvider } from "next-themes";
-import posthog from 'posthog-js';
-import { PostHogProvider } from 'posthog-js/react';
 import { ReactNode } from "react";
-
-if (typeof window !== 'undefined') {
-  posthog.init(clientEnv.NEXT_PUBLIC_POSTHOG_KEY!, {
-    api_host: clientEnv.NEXT_PUBLIC_POSTHOG_HOST,
-    person_profiles: 'always',
-  })
-}
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
-    <PostHogProvider client={posthog}>
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="dark"
-        forcedTheme="dark"
-        disableTransitionOnChange
-      >
-        {children}
-      </ThemeProvider>
-    </PostHogProvider>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="dark"
+      forcedTheme="dark"
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
   )
 }
