bca059a4f6d9765b64d3d2281046960794fe285e
Jacob Ambroz <143131645+sixtyoneeighty@users.noreply.github.com>
2025-02-04 23:20:43 -0600

    j

--- next.config.mjs
+++ next.config.mjs
@@ -49,12 +49,7 @@ const nextConfig = {
                 port: '',
                 pathname: '/s2/favicons',
             },
-            {
-                protocol: 'https',
-                hostname: 'api.producthunt.com',
-                port: '',
-                pathname: '/widgets/embed-image/v1/featured.svg',
-            },
+           
             {
                 protocol: 'https',
                 hostname: 'metwm7frkvew6tn1.public.blob.vercel-storage.com',
