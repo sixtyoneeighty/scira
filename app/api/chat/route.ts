cc10c49c6a958b3dd3d2d5169a1f297f8574c310
Jacob Ambroz <143131645+sixtyoneeighty@users.noreply.github.com>
2025-02-05 00:47:11 -0600

    d

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -105,14 +105,14 @@ export async function POST(req: Request) {
         safetySettings: [
             { category: 'HARM_CATEGORY_HATE_SPEECH', threshold: 'BLOCK_NONE' },
             { category: 'HARM_CATEGORY_DANGEROUS_CONTENT', threshold: 'BLOCK_NONE' },
-            { category: 'VHARM_CATEGORY_HARASSMENT', threshold: 'BLOCK_NONE' },
-            { category: 'ARM_CATEGORY_SEXUALLY_EXPLICIT', threshold: 'BLOCK_NONE' },
+            { category: 'HARM_CATEGORY_HARASSMENT', threshold: 'BLOCK_NONE' },
+            { category: 'HARM_CATEGORY_SEXUALLY_EXPLICIT', threshold: 'BLOCK_NONE' },
             { category: 'HARM_CATEGORY_CIVIC_INTEGRITY', threshold: 'BLOCK_NONE' },
         ],
     });
 
     const result = streamText({
-        model: model,
+        model,
         messages: convertToCoreMessages(messages),
         experimental_transform: smoothStream({
             chunking: 'word',
